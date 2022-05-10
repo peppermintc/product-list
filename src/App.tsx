d0dae458e35ca58f0eefb01ae57c18b77bc92117
박하민 <haminjp@naver.com>
2022-05-10 13:27:07 +0900

    [추가] 컴포넌트 구조 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>브랜드 필터</div>
+      <div>색상 필터</div>
+      <div>가격 필터</div>
+      <div>카테고리 필터</div>
+      <div>상품 목록</div>
+    </div>
+  );
 };
 
 export default App;
