e759f13d17629a716a1ff3b840feceb06bf8af2d
박하민 <haminjp@naver.com>
2022-05-12 13:25:35 +0900

    [수정] axios get 요청 코드 리펙토링

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,48 +1,37 @@
 import axios from "axios";
 import { Filter } from "../interfaces";
 
+const axiosGet = (path: string, params?: {}) => {
+  if (params === undefined) {
+    return axios
+      .get(
+        `https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment/${path}`
+      )
+      .then((response) => response.data)
+      .catch(console.error);
+  } else {
+    return axios
+      .get(
+        `https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment/${path}`,
+        params
+      )
+      .then((response) => response.data)
+      .catch(console.error);
+  }
+};
+
 export const axiosFetchProducts = (filter: Filter | undefined) => {
-  return axios
-    .get(
-      "https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment/products",
-      {
-        params: filter && {
-          brand: filter.brand && filter.brand.name,
-          categoryId: filter.categoryId,
-          page: filter.page,
-          color: filter.color && filter.color.name,
-          maxPrice: filter.maxPrice,
-          minPrice: filter.minPrice,
-        },
-      }
-    )
-    .then((response) => response.data)
-    .catch(console.error);
+  return axiosGet("products", {
+    params: filter && {
+      brand: filter.brand && filter.brand.name,
+      categoryId: filter.categoryId,
+      page: filter.page,
+      color: filter.color && filter.color.name,
+      maxPrice: filter.maxPrice,
+      minPrice: filter.minPrice,
+    },
+  });
 };
-
-export const axiosFetchBrands = () => {
-  return axios
-    .get(
-      "https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment/brands"
-    )
-    .then((response) => response.data)
-    .catch(console.error);
-};
-
-export const axiosFetchColors = () => {
-  return axios
-    .get(
-      "https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment/colors"
-    )
-    .then((response) => response.data)
-    .catch(console.error);
-};
-
-export const axiosFetchCategories = () => {
-  return axios
-    .get(
-      "https://lz5cdtbtci.execute-api.ap-northeast-2.amazonaws.com/assignment/categories"
-    )
-    .then((response) => response.data)
-    .catch(console.error);
-};
+export const axiosFetchBrands = () => axiosGet("brands");
+export const axiosFetchColors = () => axiosGet("colors");
+export const axiosFetchCategories = () => axiosGet("categories");
