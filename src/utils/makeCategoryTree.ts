4557494f1cb17dfea767da4536f3461e8a33e5ef
박하민 <haminjp@naver.com>
2022-05-12 14:17:08 +0900

    [추가] 동적 카테고리 트리 메이커 추가

--- src/utils/makeCategoryTree.ts
+++ src/utils/makeCategoryTree.ts
@@ -1,5 +1,26 @@
 import { Category, CategoryTreeNode } from "../interfaces";
 
+// NEW
+// 트리의 노드는 parent가 null | Category, current는 Category, children은 Category[] | Category
+
+export interface TreeNode {
+  parent: Category | null;
+  current: Category;
+  children: Category[] | Category;
+}
+
+export const newCategoryTreeMaker = (categories: Category[]) => {
+  let tree: TreeNode[] = [];
+
+  categories.forEach((category: Category) => {
+    const parent = categories.find((c) => c.id === category.parent_id) || null;
+    const children = categories.filter((c) => c.parent_id === category.id);
+    tree.push({ parent: parent, current: category, children: children });
+  });
+
+  return tree;
+};
+
 export const makeCategoryTree = (categories: Category[]) => {
   let categoryTree: CategoryTreeNode[] = [];
 
